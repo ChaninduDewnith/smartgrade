d388243602663e3e173a1b599c9e4d13f505983e
dspsilva27 <dspcdewnith@gmail.com>
2025-05-29 00:13:51 +0530

    update project

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         
       </Routes>
