4e34f0c89c6b3d69e92e3070df47240de25bccba
dspsilva27 <dspcdewnith@gmail.com>
2025-05-28 23:50:18 +0530

    adding login page

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Homepage";
+import LoginPage from "./Login";
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         
       </Routes>
     </Router>
